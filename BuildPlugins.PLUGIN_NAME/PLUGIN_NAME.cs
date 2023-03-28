294772ff0d3251a79dc6ddf69fb72f0eea251a58
Martin Gonzalez <gonzalez.martin90@gmail.com>
2023-03-28 11:25:07 +0200

    Update plugin class

--- BuildPlugins.PLUGIN_NAME/PLUGIN_NAME.cs
+++ BuildPlugins.PLUGIN_NAME/PLUGIN_NAME.cs
@@ -6,12 +6,10 @@ public class PLUGIN_NAME : IStep
 {
     public string StepId => "PLUGIN_NAMEStep";
 
-    public Task Run(BuildOrder buildOrder, IPipelineService pipelineService)
+    public async Task Run(BuildOrder buildOrder, IPipelineService pipelineService)
     {
         pipelineService.LogInfo($"Hello World! from {StepId}");
         pipelineService.LogInfo(buildOrder.ArtifactPath);
-        return Task.CompletedTask;
     }
-
     
 }
