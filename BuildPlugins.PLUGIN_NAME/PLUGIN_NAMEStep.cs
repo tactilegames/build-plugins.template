364d84c1a130f3ecd789094def0dac848d69c0bf
Martin Gonzalez <marting@tactile.dk>
2023-10-30 11:39:45 +0100

    added link to build plugins

--- BuildPlugins.PLUGIN_NAME/PLUGIN_NAMEStep.cs
+++ BuildPlugins.PLUGIN_NAME/PLUGIN_NAMEStep.cs
@@ -4,7 +4,7 @@ namespace BuildPlugins.PLUGIN_NAME;
 
 /// <summary>
 /// This file contains the simplest step you can create.
-/// If you want to know more about Options or Outputs you can go to: 
+/// If you want to know more about Options or Outputs you can go to: https://tactileentertainment.atlassian.net/wiki/spaces/GPD/pages/1291583694/Build+Plugin+Step+Class#Build-Plugin-Step-Classes-with-Requirements
 /// </summary>
 public class PLUGIN_NAMEStep : IStep
 {
